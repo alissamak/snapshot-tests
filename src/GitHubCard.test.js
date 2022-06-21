ee5856614af3bd30e939c5a4d8d92504737d444f
Alissa <alissamak@outlook.com>
2022-06-21 00:51:35 -0400

    added in tests

--- src/GitHubCard.test.js
+++ src/GitHubCard.test.js
@@ -1,10 +1,22 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-// import renderer from 'react-test-renderer';
-// import GitHubCard from './components/GitHubCard';
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App';
+import renderer from 'react-test-renderer';
+import GitHubCard from './components/GitHubCard';
 
-// test('renders a snapshot', () => {
-//     const tree = renderer.create(<GitHubCard/>).toJSON()
-//     expect(tree).toMatchSnapshot()
+test('renders a snapshot', () => {
+    const tree = renderer.create(<GitHubCard/>).toJSON()
+    expect(tree).toMatchSnapshot()
     
-//   });
+  });
+
+  beforeEach(()=> {
+    //sorts eveyrthing back to initial state before each test
+    fetch.resetMocks();
+})
+
+test('fetch gitHub profile name from GitHub REST API using jest fetch mock', async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'Alissa M'}))
+    render(<GitHubCard/>)
+    const gitHubName = await waitFor(() => screen.getByRole('card-title'))
+    expect(gitHubName).toHaveTextContent('Alissa M')
+})
